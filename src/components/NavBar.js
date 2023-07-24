428316e0f81fe2679bfbb93d1c044dff312c038e
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-24 17:09:29 +0530

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,34 @@
 // import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Nav, NavDropdown, Navbar, Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 const NavBar = () => {
 
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(
+        () => {
+            const onScroll = () => {
+                if (window.scrollY > 50) {
+                    setScrolled(true);
+                }
+                else {
+                    setScrolled(false);
+                }
+            }
+
+            window.addEventListener("scroll", onScroll);
+            return () => window.removeEventListener("scroll", onScroll);
+
+        }, [])
+
+
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src={''} alt="Logo" />
@@ -18,7 +40,7 @@ const NavBar = () => {
 
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active-navbar-link' : 'navbar-link'}>Home</Nav.Link>
                         <Nav.Link href="#skills">Skills</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
                     </Nav>
@@ -28,7 +50,7 @@ const NavBar = () => {
                             <a href="/"><img src={''} alt="" /></a>
                             <a href="/"><img src={''} alt="" /></a>
                         </div>
-                        <button className="vvd" onClick={()=>console.log('connect')}>  
+                        <button className="vvd" onClick={() => console.log('connect')}>
                             <span>Let's Connect</span>
                         </button>
                     </span>
