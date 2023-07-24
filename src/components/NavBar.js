6bff2124051135a4e880624351431840aca2f882
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-24 16:55:52 +0530

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,32 +2,39 @@
 // import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Nav,NavDropdown,Navbar, Container } from 'react-bootstrap';
+import { Nav, NavDropdown, Navbar, Container } from 'react-bootstrap';
 
 const NavBar = () => {
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="Logo" />
+                </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                {/* <span className="navbar-toggler-icon"></span> */}
+
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
                     </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="/"><img src={""} alt="" /></a>
+                            <a href="/"><img src={''} alt="" /></a>
+                            <a href="/"><img src={''} alt="" /></a>
+                        </div>
+                        <button className="vvd" onClick={()=>console.log('connect')}>  
+                            <span>Let's Connect</span>
+                        </button>
+                    </span>
+
                 </Navbar.Collapse>
+
             </Container>
         </Navbar>
     );
